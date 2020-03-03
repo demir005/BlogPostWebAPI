1bf9e0b560d55a3d3a566445552736a448cd9c05
Demir <demirmahmutovic@hotmail.com>
2020-03-03 14:33:30 +0100

    Initialize Project resources

---
+++ BlogPostAPI/Data/PostDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BlogPostAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogPostAPI.Data
+{
+    public class PostDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+}
