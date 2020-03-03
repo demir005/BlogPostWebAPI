8833e6d2f2e422f1525eda27f39998fe86015cb5
Demir <demirmahmutovic@hotmail.com>
2020-03-03 15:38:23 +0100

    Model Changed

--- BlogPostAPI/Models/Post.cs
+++ BlogPostAPI/Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,11 +11,12 @@ namespace BlogPostAPI.Models
     {
         public int Id { get; set; }
         public string Slug { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string  Description { get; set; }
+        [Required]
         public string Body { get; set; }
-
-        [NotMapped]
         public List<string> tagList { get; set; }
         public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
